2518a44b6ba83ca8ee7b0a94ef3b4bd42bf3ef7c
Zachary Honey <zhoney466@gmail.com>
2023-08-08 22:42:45 -0600

    Worked on Spring Challenge

--- frontend/components/AppFunctional.js
+++ frontend/components/AppFunctional.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, {useState} from 'react'
+import axios from 'axios'
 
 // Suggested initial states
 const initialMessage = ''
@@ -9,27 +10,79 @@ const initialIndex = 4 // the index the "B" is at
 export default function AppFunctional(props) {
   // THE FOLLOWING HELPERS ARE JUST RECOMMENDATIONS.
   // You can delete them and build your own logic from scratch.
+  const [message, setMessage] = useState(initialMessage);
+  const [email, setEmail] = useState(initialEmail);
+  const [steps, setSteps] = useState(initialSteps);
+  const [index, setIndex] = useState(initialIndex);
 
   function getXY() {
     // It it not necessary to have a state to track the coordinates.
     // It's enough to know what index the "B" is at, to be able to calculate them.
+    const cords = [
+      [1, 1],
+      [2, 1],
+      [3, 1],
+      [1, 2],
+      [2, 2],
+      [3, 2],
+      [1, 3],
+      [2, 3],
+      [3, 3],
+    ];
+    return cords[index][num];
+  }
   }
 
   function getXYMessage() {
     // It it not necessary to have a state to track the "Coordinates (2, 2)" message for the user.
     // You can use the `getXY` helper above to obtain the coordinates, and then `getXYMessage`
     // returns the fully constructed string.
+     return `Coordinates (${getXY([index],[0])}, ${getXY([num],[1])})`
   }
 
   function reset() {
     // Use this helper to reset all states to their initial values.
+    setMessage(initialMessage);
+    setEmail(initialEmail);
+    setSteps(initialSteps);
+    setIndex(initialIndex);  
   }
 
   function getNextIndex(direction) {
     // This helper takes a direction ("left", "up", etc) and calculates what the next index
     // of the "B" would be. If the move is impossible because we are at the edge of the grid,
     // this helper should return the current index unchanged.
+    if (direction === "up" && index - 3 >= 0) {
+      setMessage("");
+      setIndex(index - 3);
+      setSteps(steps + 1);
+    } else if (
+      direction === "right" &&
+      index !== 2 &&
+      index !== 5 &&
+      index !== 8
+    ) {
+      setMessage("");
+      setIndex(index + 1);
+      setSteps(steps + 1);
+    } else if (direction === "down" && index + 3 <= 8) {
+      setMessage("");
+      setIndex(index + 3);
+      setSteps(steps + 1);
+    } else if (
+      direction === "left" &&
+      index !== 0 &&
+      index !== 3 &&
+      index !== 6
+    ) {
+      setMessage("");
+      setIndex(index - 1);
+      setSteps(steps + 1);
+    } else {
+      setMessage(`You can't go ${direction}`);
+    }
   }
+  
 
   function move(evt) {
     // This event handler can use the helper above to obtain a new index for the "B",
@@ -75,4 +128,4 @@ export default function AppFunctional(props) {
       </form>
     </div>
   )
-}
+
